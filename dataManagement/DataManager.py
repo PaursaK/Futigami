e6b2518f1a575db33f0bf06cf0c1ed55457f4101
pkamalian <pkamalian98@gmail.com>
2024-08-01 18:25:52 -0700

    parsed the data table on the website to find

--- dataManagement/DataManager.py
+++ dataManagement/DataManager.py
@@ -1,9 +1,27 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
+
 url = "https://fbref.com/en/comps/9/schedule/Premier-League-Scores-and-Fixtures-html"
 page = requests.get(url)
 
-print(page.text)
+#print(page.text)
 soup = BeautifulSoup(page.text, "html.parser")
 
-print(soup.prettify)
+#print(soup.prettify)
+trList = soup.find("tbody").find_all("tr")
+firstRowInfo = trList[0].find_all("td")
+print("Column Name: " + str(firstRowInfo[0]["data-stat"]))
+print("Column Value: " + str(firstRowInfo[0].text))
+
+
+
+
+
+
+
+#print(soup.find_all(string = "home_team"))
+
+#Plan of Action
+# 1 - Retrieive appropriate table tags in order to engage with the prem league fixture table
+# 2 - translate that data to a pandas data frame
